6eb064dd925c94b73a21dc80a74eddba9a86bbcc
sarsembin <daur2000@gmail.com>
2021-11-13 19:47:13 +0600

    quicksort

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -1,5 +1,31 @@
 package quicksort
 
 func QuickSort(a []int) {
-	panic("implement me")
+	a = quicksort(a)
+}
+
+func quicksort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+
+	left, right := 0, len(a)-1
+
+	pivot := 0
+
+	a[pivot], a[right] = a[right], a[pivot]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	quicksort(a[:left])
+	quicksort(a[left+1:])
+
+	return a
 }
